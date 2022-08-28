bccc510f89b9f3f7ac1f86767cb8181f1f5cc3f7
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-08-28 13:38:35 -0300

    se agregan ultimas imagenes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EmpleadoList from "./components/EmpleadoList";
-import './App.css';
-import emp1 from './img/1.jpg'
-import emp2 from './img/2.jpg'
-import emp3 from './img/3.jpg'
-import emp4 from './img/4.jpg'
+import "./App.css";
+import emp1 from "./img/1.jpg";
+import emp2 from "./img/2.jpg";
+import emp3 from "./img/3.jpg";
+import emp4 from "./img/4.jpg";
+import emp5 from "./img/5.jpg";
+import emp6 from "./img/6.jpg";
+import emp7 from "./img/7.jpg";
+import emp8 from "./img/8.jpg";
 
 const App = () => {
   let empleados = [
-    { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", image: emp1 },
-    { id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", image: emp2 },
-    { id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", image: emp3 },
-    { id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", image: emp4 },
-    { id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "empleado05.png" },
-    { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "empleado06.png" },
-    { id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "empleado07.png" },
-    { id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "empleado08.png" },
-    { id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "empleado09.png" },
-  ]
-  
+    {
+      id: 1,
+      fullName: "Laya Dueñas",
+      title: "CEO",
+      department: "Business",
+      image: emp1,
+    },
+    {
+      id: 2,
+      fullName: "Astryd Vallés",
+      title: "CMO",
+      department: "Marketing",
+      image: emp3,
+    },
+    {
+      id: 3,
+      fullName: "Shantell Meza",
+      title: "CFO",
+      department: "Business",
+      image: emp5,
+    },
+    {
+      id: 4,
+      fullName: "Sergio Ocampo",
+      title: "CTO",
+      department: "Engineering",
+      image: emp2,
+    },
+    {
+      id: 5,
+      fullName: "Ares Jiménez",
+      title: "Art Director",
+      department: "Marketing",
+      image: emp4,
+    },
+    {
+      id: 6,
+      fullName: "Marta Pérez",
+      title: "Frontend Dev",
+      department: "Engineering",
+      image: emp6,
+    },
+    {
+      id: 7,
+      fullName: "Jason One",
+      title: "Digital Strategist",
+      department: "Marketing",
+      image: emp7,
+    },
+    {
+      id: 8,
+      fullName: "Bernard Jung",
+      title: "DevOps Engineer",
+      department: "Engineering",
+      image: emp8,
+    },
+  ];
+
   return (
     <div className="container">
       <EmpleadoList empleados={empleados}></EmpleadoList>
